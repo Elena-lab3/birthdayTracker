baf386aa0757ac2d1d0fe25da1fcbc3a39b02e7c
Elena-lab3 <barkovskaaelena3@gmail.com>
2022-02-19 21:59:34 +0300

    i'm predurashnaya

---
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  BirthdayTracker
+//
+//  Created by Алёнка on 23.12.21.
+//
+
+import UIKit
+import CoreData
+import UserNotifications
+
+class AddBirthdayViewController: UIViewController {
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet var birthdatePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        birthdatePicker.maximumDate = Date()
+    }
+    @IBAction func saveTapped( _sender: UIBarButtonItem){
+        let fN = firstNameTextField.text ?? "who"
+        let lN = lastNameTextField.text ?? "knows"
+        let bD = birthdatePicker.date
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newBirthday = Birthday(context: context)
+        newBirthday.firstName = fN
+        newBirthday.lastName = lN
+        newBirthday.birthdaydate = bD as Date?
+        newBirthday.birthdayId = UUID().uuidString
+        if let uniqueId = newBirthday.birthdayId {
+            print("birthdayId: \(uniqueId)")
+        }
+        
+        do{
+            try context.save()
+            let message = "Сегодня \(fN) \(lN) празднует 8 день рождения!"
+            let content = UNMutableNotificationContent()
+                content.body = message
+            content.sound = UNNotificationSound.default
+            var dateComponents = Calendar.current.dateComponents([.month, .day], from: bD)
+            dateComponents.hour = 8
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+            if let identifier = newBirthday.birthdayId {
+            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                   let center = UNUserNotificationCenter.current()
+                   center.add(request, withCompletionHandler: nil)
+            }
+        } catch let error {
+            print("Не удалось сохранить из-за ошибки \(error).")
+            
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
